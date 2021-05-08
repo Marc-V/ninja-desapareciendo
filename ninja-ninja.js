8d77fd1cf293ef318886e493e0a8468aa0bf2828
Marc-V <mvsquez9@gmail.com>
2021-05-07 20:55:42 -0400

    Ninja Desapareciendo

---
+++ ninja-ninja.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    $("#btn-restore").click(function(){
+        $(".one, .two, .three, .four, .five, .six, .seven, .eight").show();
+    });
+
+    $(".one").click(function(){
+        $(".one").hide();
+    });
+
+    $(".two").click(function(){
+        $(".two").hide();
+    });
+
+    $(".three").click(function(){
+        $(".three").hide();
+    });
+
+    $(".four").click(function(){
+        $(".four").hide();
+    });
+
+    $(".five").click(function(){
+        $(".five").hide();
+    });
+    
+    $(".six").click(function(){
+        $(".six").hide();
+    });
+
+    $(".seven").click(function(){
+        $(".seven").hide();
+    });
+
+    $(".eight").click(function(){
+        $(".eight").hide();
+    });
+});
